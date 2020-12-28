182be0389e4876536be918ab91a4c0f06e412409
alimovlex <alimovlex@hotmail.com>
2020-12-29 02:58:45 +0300

    The Object Oriented Features have been added!

--- Sources/Sandbox_Apple/main.swift
+++ Sources/Sandbox_Apple/main.swift
@@ -12,9 +12,20 @@ class Weekdays
     init() {
         print("Constructing");
     }
-        var days: [Int: String] = [1: "Monday", 2: "Tuesday", 3: "Wednesday",
-                                   4: "Thursday", 5: "Friday", 6: "Saturday", 7: "Sunday"];
-
+        var days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday",
+                              "Friday", "Saturday", "Sunday"];
+    var quantity:Int = 7;
+    subscript(index: Int) -> String
+    {
+        get
+        {
+            return days[index];
+        }
+        set (newValue)
+        {
+            self.days[index] = newValue;
+        }
+    }
         func summ(a: Float, b: Float) -> Float {
             return a + b;
         }
@@ -25,35 +36,49 @@ class Weekdays
 
 class Months: Weekdays
 {
-    var months: [Int:String] = [1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 
-                                7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"];
+    var months: [String] = ["January", "February", "March", "April", "May", "June", "July", 
+                            "August", "September", "October", "November", "December"];
+    override var quantity: Int {
+        get {
+            return super.quantity;
+        }
+        set {
+            super.quantity = 12;
+        }
+    }
 
     override func summ(a: Float, b: Float) -> Float {
         return super.summ(a: pow(a, 2.0), b: pow(b, 2));
     }
 }
 
-func tutorial ()
+struct Today
+{
+    var word: String;
+    var wort: String;
+    var year: Int;
+}
+
+func tutorial (entrance:String, exit:String) -> String
 {
 
     let pi = 3.14159, c = 2.0, string = String("Hello World!"); //the constants
     var error501 = (501, "Not Implemented"); //The Tuple variable
     var error403 = (403, "Forbidden"); //The Tuple variable
     var error404 = (404, "Not Found"); //The Tuple variable
-    var word: String, year: Int, wort:String;
+    var heute = Today(word:"Year", wort:"New ", year:2021);
     var array = [Int](repeating: 4, count: 3); //an array of repeating integers
     var massiv: [Int] = [1,2,3,4,5]; // an array of ints
     var names: Set<String> = ["David", "Joe", "Sean"];
     var namen: [String:String];
     var days = Weekdays(), months = Months(), today = Days.Monday;//constructing objects
     today = .Monday;
-    word = "Year";
-    wort = "New ";
-    year = 2021;
-    namen = ["David":"Jones", "Joe":"Silverman", "Sean":"Davis"];
-
-    print((string), (wort)+(word),days.summ(a: 2000, b: 21));
-    print("string word counts = \(word.count)\(wort.count)");
+    namen = ["David":"Jones", "Joe":"Silverman", "Sean":"Davis"]; //Declaring dictionary
+    namen.updateValue("Silberman", forKey: "Joe"); //Changing the dictionary
+    namen.updateValue("Beckham", forKey: "David"); //Modifying the dictionary
+    namen.updateValue("Hernandez", forKey: "Sean");//Editing the dictionary
+    print((string),"\n", (heute.wort)+(heute.word),days.summ(a: 2000, b: 21));
+    print("string word counts = \(heute.word.count) \(heute.wort.count)");
     massiv.removeLast();
     print(array, massiv);
     for names in names.sorted()
@@ -61,7 +86,12 @@ func tutorial ()
         print(names);
     }
 
-    switch year {
+    for namen in namen
+    {
+        print(namen);
+    }
+
+    switch heute.year {
     case 2000...2010:
         print("In a first decade");
     case 2010...2020:
@@ -78,20 +108,20 @@ func tutorial ()
     switch today
     {
     case .Monday:
-        print("Today's Monday");
+        print("Today's \(days[0])");
         //fallthrough; falling to another statement
     case .Tuesday:
-        print("Today's Tuesday");
+        print("Today's \(days[1])");
     case .Wednesday:
-        print("Today's Wednesday");
+        print("Today's \(days[2])");
     case .Thursday:
-        print("Today's Thursday");
+        print("Today's \(days[3])");
     case .Friday:
-        print("Today's Friday");
+        print("Today's \(days[4])");
     case .Saturday:
-        print("Today's Saturday");
+        print("Today's \(days[5])");
     case .Sunday:
-        print("Today's Sunday");
+        print("Today's \(days[6])");
     default:
         print("The day is incorrect");
         break;
@@ -99,30 +129,21 @@ func tutorial ()
 
     let studname = { print("Welcome to Swift Closures") } //embedded function in tutorial()
     studname();
+    return entrance+exit;
 }
-
-struct rectangle {
-    var length: Double;
-    var breadth: Double;
-    init()
-    {
-        length = 6;
-        breadth = 12;
-    }
-}
-
 
 
 func sandbox()
 {
-    var area = rectangle();
-    print("area of rectangle is \(area.length*area.breadth)");
+
 }
 
 func main()
 {
+    var demo: (String, String) -> String = tutorial;
+    print(tutorial(entrance: "Learning ", exit: "Swift"));
     //tutorial();
-    sandbox();
+    //sandbox();
 }
 
 main();
