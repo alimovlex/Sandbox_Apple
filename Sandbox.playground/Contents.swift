7ccef47657f8abf5c13c46a8e0b24b0d6b6e18e3
alimovlex <alimovlex@hotmail.com>
2021-02-01 01:17:39 +0600

    The data structure "set" has been discovered.

--- Sandbox.playground/Contents.swift
+++ Sandbox.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 func main()
 {
-    var str = "Hello world";
+    var str = ["Hello", "world"];
     //the courtage block definition
     var tuple1 = (200, "In work", true);
     var tuple2 = (true, "At Work", 200);
@@ -11,9 +11,25 @@ func main()
     let alphabetArray = ["a", "b", "c"]; //first method
     let newalphabetArray = Array(arrayLiteral: 2,4,8,1); //second method
     let lineArray = Array(0...9); //third method
+    let repeatArray = Array(repeating: "Swift", count: 5);
     var mutableArray = [2,4,8,1]; //the default changeable array
+    var emptyArray: [String] = []; //first way
+    var anotherEmptyArray = [String](); //second way
+    str+=repeatArray; //the array appension
+    var matrix = [[1,2,3],[4,5,6],[7,6,8]]; //the array-matrice
+    //the set definition ways
+    var dishes: Set<String> = ["Bread", "Vegetables", "Beef", "Water"];
+    var dishesTwo: Set = ["Bread", "Vegetables", "Beef", "Water"];
+    var members = Set<String>(arrayLiteral: "Chicken", "Fish", "Meat");
+    var membersTwo = Set<String>(arrayLiteral: "Chicken", "Fish", "Meat");
+    var emptySet = Set<String>();
     //the courtage block use
     print("Hello, playground\n\(str)");
+    print("""
+        \(matrix[0])
+        \(matrix[1])
+        \(matrix[2])
+"""); //the multiline output
     print(type(of:tuple1) == type(of:tuple2)); //the comparison of two coutages
     print("The response code: \(statusTuple.statusCode) | Duplicating:\(statusTuple.0)");
     print("The text code: \(statusTuple.statusText) | Duplicating:\(statusTuple.1)");
