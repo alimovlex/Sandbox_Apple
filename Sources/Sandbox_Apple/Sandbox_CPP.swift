6a6abf2a138e6c40e65368563b1e3b10e49dc0f8
alimovlex <alimovlex@hotmail.com>
2021-06-05 20:55:56 +1000

    The wrappers for C++ deque container has been added to the sandbox.

--- Sources/Sandbox_Apple/Sandbox_CPP.swift
+++ Sources/Sandbox_Apple/Sandbox_CPP.swift
@@ -5,20 +5,26 @@
 import Foundation
 
 func cppListTest () {
-    let list = SwiftListWrapper(dataSize: 1)
-    list.push_back(value: "1".data(using: .utf8)!)
-    list.push_back(value: "2".data(using: .utf8)!)
-    list.push_back(value: "3".data(using: .utf8)!)
-    list.push_front(value: "0".data(using: .utf8)!)
+    let list = SwiftListWrapper(dataSize: 1);
+    list.push_back(value: "1".data(using: .utf8)!);
+    list.push_back(value: "2".data(using: .utf8)!);
+    list.push_back(value: "3".data(using: .utf8)!);
     print("The size of C++ List \(list.size())");
 
-    let vector = SwiftVectorWrapper(dataSize: 1)
-    vector.push_back(value: "1".data(using: .utf8)!)
-    vector.push_back(value: "2".data(using: .utf8)!)
-    vector.push_back(value: "3".data(using: .utf8)!)
+    let vector = SwiftVectorWrapper(dataSize: 1);
+    vector.push_back(value: "1".data(using: .utf8)!);
+    vector.push_back(value: "2".data(using: .utf8)!);
+    vector.push_back(value: "3".data(using: .utf8)!);
     print("The size of C++ Vector \(vector.size())");
 
-    let forward_list = SwiftForwardListWrapper(dataSize: 1)
+    let deque = SwiftDequeWrapper(dataSize: 1);
+    deque.push_back(value: "1".data(using: .utf8)!);
+    deque.push_back(value: "2".data(using: .utf8)!);
+    deque.push_back(value: "3".data(using: .utf8)!);
+    print("The size of C++ Deque \(deque.size())");
+
+    let forward_list = SwiftForwardListWrapper(dataSize: 1);
     print("The C++ ForwardList is empty? The answer is :\(forward_list.empty())");
+
 }
 
