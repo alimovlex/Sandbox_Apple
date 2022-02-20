7778779109771714134e2a23356d297cc74164a1
alimovlex <alimovlex@hotmail.com>
2022-02-20 22:46:27 +1000

    Eradicated the redundant Test target in the Sandbox,

--- Package.swift
+++ Package.swift
@@ -7,9 +7,6 @@ let package = Package(
     name: "Sandbox_Apple",
         products: [
             // Products define the executables and libraries produced by a package, and make them visible to other packages.
-            .library(
-                    name: "Sandbox_CPP",
-                    targets: ["Sandbox_CPP"]),
         ],
     dependencies: [
         //.package(url: "https://github.com/belozierov/SwiftCoroutine", .upToNextMajor(from: "2.1.11"))
@@ -25,14 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-                name: "Sandbox_CPP",
-                path: "Sources/Sandbox_CPP"
-        ),
-        .target(
             name: "Sandbox_Apple",
-            dependencies: ["Sandbox_CPP", "Kitura", "Kitura-WebSocket"]),
-        .testTarget(
-            name: "Sandbox_AppleTests",
-            dependencies: ["Sandbox_Apple"]),
+            dependencies: ["Kitura", "Kitura-WebSocket"]),
     ]
 )
